251d00ccaa3245db7daf6eec892b4eccdc49bd4a
Kaiseridze <kaiseidze@gmail.com>
2022-05-16 10:41:43 +0300

    create comments and refactor view-side

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const inputFrom = document.getElementById("from")
 const inputTo = document.getElementById("to")
 const inputSubmit = document.querySelector(".submit")
 
+// converting all nulls to 0
 courses.forEach(course => {
 	if (course.prices[0] === null) {
 		course.prices[0] = 0
@@ -17,6 +18,7 @@ courses.forEach(course => {
 const main = document.querySelector(".container")
 main.appendChild(ul)
 
+// Main algorithm
 const filterItem = (range, range2) => {
 	if (!range2) range2 = 500
 	if (!range) range = 0
@@ -26,18 +28,15 @@ const filterItem = (range, range2) => {
 			course.prices[0] <= range2 &&
 			course.prices[1] <= range2
 	)
-	return ul.innerHTML = (filteredCourse.map((item) => `<li>${item.name} ${item.prices}</li>`))
+	return (ul.innerHTML = filteredCourse.map(
+		(item) => `<li>${item.name} ${item.prices[0]} - ${item.prices[1]} usd</li>`
+	))
 }
 filterItem()
 
-
+// Change value and submit
 inputSubmit.addEventListener("click", (e) => {
 	e.preventDefault
 	filterItem(inputFrom.value, inputTo.value)
 })
-
-
-
-
-	
 
